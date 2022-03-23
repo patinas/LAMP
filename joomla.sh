eb628b444ae2abb28134d179fcc0354da0c37e5d
patinas <andreas.patinas@gmail.com>
2022-03-23 12:55:19 +0000

    M joomla.sh

--- joomla.sh
+++ joomla.sh
@@ -9,8 +9,8 @@ sudo apt install docker.io docker-compose -y
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo chmod +x joomla_docker.sh
+sudo chmod +x *.sh
 sudo docker pull joomla
 sudo docker pull mysql
 sudo docker run --name joomla_db -d -e MYSQL_ROOT_PASSWORD=test mysql
-sudo docker run --name joomla_website --link joomla_db:mysql -d -p 80:80 joomla
+sudo docker run --name joomla_website --link joomla_db:mysql -d -p 81:80 joomla
