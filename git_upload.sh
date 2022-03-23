f863149782c26a00f2a3029335ed9f3e76f21ec3
patinas <andreas.patinas@gmail.com>
2022-03-23 08:47:27 +0000

    A  git_upload.sh

---
+++ git_upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo chmod +x *.sh
+gstatus=`git status --porcelain`
+
+if [ ${#gstatus} -ne 0 ]
+then
+
+    git add --all
+    git commit -m "$gstatus"
+    git pull
+    git push
+
+fi
+
+cat <<EOF | crontab -
+*/30 * * * * /root/post_install_linux/update.sh
+*/3 * * * * /root/config/git_upload.sh
+EOF
