5033077df2844d6737467a99fb224f3663832eb2
patinas <andreas.patinas@gmail.com>
2022-03-23 09:50:44 +0000

    M apache.sh

--- apache.sh
+++ apache.sh
@@ -46,7 +46,6 @@ sudo cat > /var/www/demo1.test/html/index.html  <<EOF
 <html>
 <body>
     <h1>Welcome to DEMO 1</h1>
-    <p>This is the first website on the Apache web server</p>
 </body>
 </html>
 EOF
@@ -57,7 +56,6 @@ sudo cat > /var/www/demo2.test/html/index.html  <<EOF
 <html>
 <body>
     <h1>Welcome to DEMO 2</h1>
-    <p>This is the first website on the Apache web server</p>
 </body>
 </html>
 EOF
@@ -76,7 +74,7 @@ EOF
 sudo cat > /etc/apache2/sites-available/demo2.conf  <<EOF
 <VirtualHost *:80>
     ServerName demo2.test
-    DocumentRoot /var/www/demo1.test/html
+    DocumentRoot /var/www/demo2.test/html
 </VirtualHost>
 EOF
 
@@ -84,25 +82,3 @@ EOF
 a2ensite demo1.conf
 a2ensite demo2.conf
 systemctl reload apache2
-
-
-
-
-
-
-# Proxy
-sudo a2enmod proxy
-sudo a2enmod proxy_http
-sudo a2enmod proxy_balancer
-sudo a2enmod lbmethod_byrequests
-
-sudo systemctl restart apache2
-
-sudo cat > /etc/apache2/sites-available/000-default.conf  <<EOF
-<VirtualHost *:80>
-    ProxyPreserveHost On
-
-    ProxyPass / http://127.0.0.1:80/
-    ProxyPassReverse / http://127.0.0.1:80/
-</VirtualHost>
-EOF
